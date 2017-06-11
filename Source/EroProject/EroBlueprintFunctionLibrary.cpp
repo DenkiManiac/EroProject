72984e255e72120237199ae7bf34425e257ffd18
DenkiManiac <DenkiManiac@users.noreply.github.com>
2017-06-11 17:55:02 +0200

    fix include compilation error

--- Source/EroProject/EroBlueprintFunctionLibrary.cpp
+++ Source/EroProject/EroBlueprintFunctionLibrary.cpp
@@ -1,10 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "EroBlueprintFunctionLibrary.h"
 #include "FileManager.h"
 #include "PlatformFilemanager.h"
 #include "FileHelper.h"
 #include "EroProject.h"
-#include "EroBlueprintFunctionLibrary.h"
+
 
 TArray<FString> UEroBlueprintFunctionLibrary::AbrirFichero(FString r)
 {
